958cbf1865deb2c2fe3694d702b30d2b38e87136
Aaron Ayerdis <ayerdis.espinoza.aa@gmail.com>
2021-11-05 15:18:49 -0600

    7. Add headers json and home content boxes modifies

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -1,7 +1,32 @@
-class PanelController < ApplicationController
-  def index
-    summary = File.read('app/assets/json/summary.json')
+require 'json'
 
-    @total_sales = 3000
+class PanelController < ApplicationController
+  def index    
+
+    # Fetching json file for numbers
+    file_info = File.read('app/assets/json/summary.json')
+    data_info =  JSON.parse(file_info)
+
+    # Fetching json file for headers
+    file_headers = File.read('app/assets/json/headers.json')
+    data_headers = JSON.parse(file_headers)
+   
+    # Loading headers into global variables
+    @total_rent_header = data_headers["rent"]["quantity"]
+    @type_rent_header = data_headers["rent"]["type"]
+    @avg_rent_header = data_headers["rent"]["price"]["avg"]
+
+    @total_sale_header = data_headers["sale"]["quantity"]
+    @type_sale_header = data_headers["sale"]["type"]
+    @avg_sale_header = data_headers["sale"]["price"]["avg"]
+
+
+    # Loading info into global variables
+    @total_rent = data_info["rent"]["count"]
+    @avg_rent = data_info["rent"]["price"]["avg"]
+
+    @total_sale = data_info["sale"]["count"]
+    @avg_sale = data_info["sale"]["price"]["avg"]
+
   end
 end
